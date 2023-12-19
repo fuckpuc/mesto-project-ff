5f0e2e14a8bcfdc5e2ffcd00eefc1c2caec07ca9
karen159357 <jokerhambardzumyan@gmail.com>
2023-12-20 00:44:45 +0300

    First Reviyu

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
 // @todo: Функция удаления карточки
+function deleteElement() {
+  const card = document.querySelector(".card");
+  card.remove();
+}
 
-// @todo: Вывести карточки на страницу
+const cardTemplate = document.querySelector("#card-template").content;
+const placesList = document.querySelector(".places__list");
+
+function addElement({ name, link }) {
+  const card = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = card.querySelector(".card__image");
+  const cardTitle = card.querySelector(".card__title");
+  const delButton = card.querySelector(".card__delete-button");
+  cardImage.src = link;
+  cardTitle.innerText = name;
+  delButton.addEventListener("click", deleteElement);
+  return card;
+}
+
+function renderCards() {
+  initialCards.forEach(function (card) {
+    const cardElement = addElement(card, deleteElement);
+    placesList.append(cardElement);
+  });
+}
+renderCards();
